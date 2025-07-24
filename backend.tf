dd00071b754714d23e23d4237e005018303f89dd
Cheulong Sear <searcheulong@gmail.com>
2025-07-24 08:19:00 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "sfjljw34-terraform-remote-state"
-    key    = "terraform.tfstate"
-    region = "ap-southeast-1"
-    use_lockfile = true
-    encrypt = true
-    }
+  # backend "s3" {
+  #   bucket = "sfjljw34-terraform-remote-state"
+  #   key    = "terraform.tfstate"
+  #   region = "ap-southeast-1"
+  #   use_lockfile = true
+  #   encrypt = true
+  #   }
 }
