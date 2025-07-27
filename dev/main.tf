efb4f9a862d8575ddffd149df028859780000252
Cheulong Sear <searcheulong@gmail.com>
2025-07-27 15:43:24 +0700

    update: gitlab-ci.yml

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,25 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "cheulong-devops-terraform-state-kljbxs"
+    key            = "aws-photo-compressor/dev/terraform.tfstate"
+    region         = "us-east-1"
+    
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 
 module "vpc" {
   source      = "../modules/vpc"
