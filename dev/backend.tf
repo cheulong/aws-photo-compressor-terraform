130edaadcfc99fc4be9be2ca3ad13beccb45c806
Cheulong Sear <searcheulong@gmail.com>
2025-07-27 15:48:25 +0700

    update: gitlab-ci.yml

---
+++ dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+    bucket       = "cheulong-devops-terraform-state-kljbxs"
+    key          = "aws-photo-compressor/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
--- dev/main.tf
+++ dev/main.tf
@@ -6,13 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket       = "cheulong-devops-terraform-state-kljbxs"
-    key          = "aws-photo-compressor/dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
+
 }
 
 # Configure the AWS Provider
