484b95f129106e608cf93a637d355b55dd61441e
Cheulong Sear <searcheulong@gmail.com>
2025-07-27 15:46:30 +0700

    update: gitlab-ci.yml

--- dev/main.tf
+++ dev/main.tf
@@ -7,10 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "cheulong-devops-terraform-state-kljbxs"
-    key            = "aws-photo-compressor/dev/terraform.tfstate"
-    region         = "us-east-1"
-    
+    bucket       = "cheulong-devops-terraform-state-kljbxs"
+    key          = "aws-photo-compressor/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
