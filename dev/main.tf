e5fbd6ccfcb409e40f21798d9a1cdddebbd23683
Cheulong Sear <searcheulong@gmail.com>
2025-07-27 16:44:17 +0700

    update: gitlab-ci.yml

--- dev/main.tf
+++ dev/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-
-
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-
 module "vpc" {
   source      = "../modules/vpc"
   environment = var.environment
