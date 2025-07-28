f41c1fae793f5b124bd558a65334507360ce09e0
Cheulong Sear <searcheulong@gmail.com>
2025-07-29 02:46:37 +0700

    update: dev vpc

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-    bucket       = "cheulong-devops-terraform-state-kljbxs"
+    bucket       = "cheulong-devops-terraform-state-6zshsv"
     key          = "aws-photo-compressor/dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
