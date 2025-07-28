ec7d09b572c95bedbdafa6b885d030366b21eaf6
Cheulong Sear <searcheulong@gmail.com>
2025-07-29 03:04:33 +0700

    update: staging vpc

---
+++ staging/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+    bucket       = "cheulong-devops-terraform-state-6zshsv"
+    key          = "aws-photo-compressor/staging/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
