573302008e8d94787fee3d4f44a1e044ca2079be
Cheulong Sear <searcheulong@gmail.com>
2025-07-29 23:05:13 +0700

    add: s3 bucket prod

--- prod/main.tf
+++ prod/main.tf
@@ -1,6 +1,10 @@
-
 module "vpc" {
   source      = "../modules/vpc"
   environment = var.environment
   cidr_block  = var.cidr_block
+}
+
+module "s3" {
+  source = "../modules/s3"
+  environment = var.environment
 }
--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "s3_id" {
+  value = module.s3.s3_id
+}
