cb6377120bfc804879de75d170a805b75dba264f
Cheulong Sear <searcheulong@gmail.com>
2025-08-01 01:04:40 +0700

    update: s3 module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -7,6 +7,33 @@ resource "aws_s3_bucket" "photo_bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "photo_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.photo_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "photo_bucket_public_access_block" {
+  bucket = aws_s3_bucket.photo_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "photo_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.photo_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.photo_bucket_public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.photo_bucket.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "photo_bucket_encryption" {
   bucket = aws_s3_bucket.photo_bucket.id
 
